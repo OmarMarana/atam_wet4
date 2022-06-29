f342ebf61494987e37137f8bf199243f3f006417
Omar Marana <omarmarana@campus.technion.ac.il>
2022-06-29 12:24:03 +0000

    passed some tests

--- prf.c
+++ prf.c
@@ -17,36 +17,42 @@
 #define ET_EXEC 2
 
 
-void* check_if_file_is_valid(char* file_name) 
+void* check_if_file_is_valid(const char* file_name) 
 {
     FILE* ELF = fopen(file_name, "r");
     char magic [4];
-    if (!ELF) {
+    if (!ELF) 
+    {
+        //printf("cheching if open succeeded %s\n", file_name);
         return NULL;
     }
     if (fread(magic, 1, 4, ELF) < 4 || magic[0] != 127 || magic[1] != 'E' || magic[2] != 'L' || magic[3] != 'F') 
     {
+        //printf("cheching if elf\n");
         fclose(ELF);
         return NULL;
     }
     int elf_fd = open(file_name, O_RDONLY);
     if (elf_fd == -1) 
     {
+        //printf("cheching open filename\n");
         fclose(ELF);
         return NULL;
     }
     void *elf = mmap(NULL, lseek(elf_fd, 0, SEEK_END), PROT_READ, MAP_PRIVATE, elf_fd, 0);
     Elf64_Ehdr* elf_header = (Elf64_Ehdr*)elf;
-    if (elf_header->e_type != ET_EXEC) {
+    if (elf_header->e_type != ET_EXEC) 
+    {
         printf("PRF:: <prog name> not an executable! :(\n");
         fclose(ELF);
         close(elf_fd);
         return NULL;
     }
+    //printf("file is valid\n");
     return elf;
 }
 
-bool check_file_and_func(char* file_name, char* func_name, Elf64_Half* sec_index_of_func, unsigned long* addr) 
+bool check_file_and_func(const char* file_name, const char* func_name, Elf64_Half* sec_index_of_func, unsigned long* addr) 
 {
     void* elf = check_if_file_is_valid(file_name);
     if (!elf) 
@@ -74,9 +80,7 @@ bool check_file_and_func(char* file_name, char* func_name, Elf64_Half* sec_index
             strtab = ((char*)elf + section_h_arr[i].sh_offset);
         }
     }
-
     bool found = false;
-
     for(int i = 0; i < symbols_amount; i++)
     {
         char* curr_symbol_name = strtab + symtab[i].st_name;
@@ -92,7 +96,8 @@ bool check_file_and_func(char* file_name, char* func_name, Elf64_Half* sec_index
             }
         }
     }
-    if (!found) {
+    if (!found) 
+    {
         printf("PRF:: <function name> not found!\n");
         return false;
     }
@@ -101,7 +106,7 @@ bool check_file_and_func(char* file_name, char* func_name, Elf64_Half* sec_index
 }
 
 
-unsigned long find_GOT_entry(char* file_name, char* func_name)
+unsigned long find_GOT_entry(const char* file_name, const char* func_name)
 { 
     int elf_fd = open(file_name, O_RDONLY);
     void *elf = mmap(NULL, lseek(elf_fd, 0, SEEK_END), PROT_READ, MAP_PRIVATE, elf_fd, 0);
@@ -142,17 +147,15 @@ unsigned long find_GOT_entry(char* file_name, char* func_name)
            break; 
         }
     }
-    //i dont think this works, since we cant intervene with the childs mem space
-    // return *((unsigned long*)(rela_plt[i].r_offset));
     return rela_plt[i].r_offset;
 }
 
 
-void run_sys_debugger(pid_t child_pid, unsigned long func_addr, Elf64_Half UND, char* file_name, char* func_name) 
+void run_sys_debugger(pid_t child_pid, unsigned long func_addr, Elf64_Half UND, const char* file_name, const char* func_name) 
 {
     int wait_status;
     struct user_regs_struct regs; 
-    unsigned long curr_rsp;
+    unsigned long long int wanted_rsp;
     unsigned long r_a;
     unsigned long func_addr_GOT_entry;
     int call_counter = 0;
@@ -165,25 +168,28 @@ void run_sys_debugger(pid_t child_pid, unsigned long func_addr, Elf64_Half UND, 
         func_addr = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)func_addr_GOT_entry, NULL);
     } 
 
-    while(WIFEXITED(wait_status))
+    long func_start_data = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)func_addr, NULL);
+    // define data_trap
+    unsigned long func_start_data_trap = (func_start_data & 0xFFFFFFFFFFFFFF00) | 0xCC;
+
+    while(!WIFEXITED(wait_status))
     {
-        long func_start_data = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)func_addr, NULL);
-        // define data_trap
-        unsigned long func_start_data_trap = (func_start_data & 0xFFFFFFFFFFFFFF00) | 0xCC;
-        // adding the first BP
+        // adding BP at func
         ptrace(PTRACE_POKETEXT ,child_pid, (void*)func_addr, (void*)func_start_data_trap);
-        
         ptrace(PTRACE_CONT, child_pid, NULL, NULL);
         wait(&wait_status);
+        if (WIFEXITED(wait_status)) 
+        {
+            return;
+        }
         //child stopped at breakpoint at the start of function
-        ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
-        
+        ptrace(PTRACE_GETREGS, child_pid, NULL, &regs);
         ptrace(PTRACE_POKETEXT, child_pid, (void*)func_addr, (void*)func_start_data);
         regs.rip -= 1;
-        curr_rsp = regs.rsp;
-        ptrace(PTRACE_SETREGS, child_pid, 0, &regs);
+        wanted_rsp = regs.rsp;
+        ptrace(PTRACE_SETREGS, child_pid, NULL, &regs);
         //does peek read towards higher addresses?
-        r_a = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)curr_rsp, NULL);
+        r_a = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)wanted_rsp, NULL);
         long r_a_data = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)r_a, NULL);
         unsigned long r_a_data_trap = (r_a_data & 0xFFFFFFFFFFFFFF00) | 0xCC;
         ptrace(PTRACE_POKETEXT ,child_pid, (void*)r_a, (void*)r_a_data_trap);
@@ -191,61 +197,49 @@ void run_sys_debugger(pid_t child_pid, unsigned long func_addr, Elf64_Half UND, 
         ptrace(PTRACE_CONT, child_pid, NULL, NULL);
         wait(&wait_status);
 
-        ptrace(PTRACE_POKETEXT, child_pid, (void*)func_addr, (void*)r_a_data);
-        ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+        ptrace(PTRACE_POKETEXT, child_pid, (void*)r_a, (void*)r_a_data);
+        ptrace(PTRACE_GETREGS, child_pid, NULL, &regs);
         regs.rip -= 1;
-        ptrace(PTRACE_SETREGS, child_pid, 0, &regs);
-        
         // at this point the child is stopped at the r_a
-        while(WIFSTOPPED(wait_status))
+        unsigned long long int curr_rsp = regs.rsp - 8;
+        int counter = 0;
+        while(wanted_rsp != curr_rsp)
         {
-            ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
-            if (r_a == regs.rip && regs.rsp == curr_rsp )
+            ptrace(PTRACE_SETREGS, child_pid, NULL, &regs);
+            // do one instruction
+            if (ptrace(PTRACE_SINGLESTEP, child_pid, NULL, NULL) < 0)
             {
-                call_counter++;
-                printf("PRF:: run #%d returned with %lld\n", call_counter, regs.rax);
-                if (UND == SHN_UNDEF)
-                {
-                    func_addr = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)func_addr_GOT_entry, NULL);
-                }
-                break;
+                perror("ptrace");
+                return;
             }
-
-            ptrace(PTRACE_POKETEXT ,child_pid, (void*)func_addr, (void*)func_start_data_trap);
+            // return BP to r_a, wait for it to reach,
+            ptrace(PTRACE_POKETEXT, child_pid, (void*)r_a, (void*)r_a_data_trap);
             ptrace(PTRACE_CONT, child_pid, NULL, NULL);
             wait(&wait_status);
-            //child stopped at breakpoint at the start of function
-            ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
-            ptrace(PTRACE_POKETEXT, child_pid, (void*)func_addr, (void*)func_start_data);
+
+            //child stopped at breakpoint at r_a
+            ptrace(PTRACE_GETREGS, child_pid, NULL, &regs);
+            ptrace(PTRACE_POKETEXT, child_pid, (void*)r_a, (void*)r_a_data);
             regs.rip -= 1;
-            ptrace(PTRACE_SETREGS, child_pid, 0, &regs);
-            //does peek read towards higher addresses?
-            unsigned long r_a_not_first_call = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)regs.rsp, NULL);
-            long r_a_data = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)r_a_not_first_call, NULL);
-            unsigned long r_a_data_trap = (r_a_data & 0xFFFFFFFFFFFFFF00) | 0xCC;
-            ptrace(PTRACE_POKETEXT ,child_pid, (void*)r_a_not_first_call, (void*)r_a_data_trap);
-
-            ptrace(PTRACE_CONT, child_pid, NULL, NULL);
-            wait(&wait_status);
-            
-
-            ptrace(PTRACE_POKETEXT, child_pid, (void*)func_addr, (void*)r_a_data);
-            regs.rip -= 1;
-            ptrace(PTRACE_SETREGS, child_pid, 0, &regs);
-
+            curr_rsp = regs.rsp - 8;
         }
+        ptrace(PTRACE_SETREGS, child_pid, NULL, &regs);
+        call_counter++;
+        printf("PRF:: run #%d returned with %lld\n", call_counter, regs.rax);
+        if (UND == SHN_UNDEF)
+        {
+            func_addr = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)func_addr_GOT_entry, NULL);
+            UND = 1;
+        }
+        
     }
-    
-    
-    
-    
-
-
-    
+    printf("after while\n");
 }
 
 
-pid_t run_target(const char* exefile_name, char** argv){
+pid_t run_target(char** argv) 
+{
+    //printf("running target\n");
     pid_t pid = fork();
     if (pid > 0)
     {
@@ -258,7 +252,8 @@ pid_t run_target(const char* exefile_name, char** argv){
 			perror("ptrace");
 			exit(1);
         }
-		execl(exefile_name, *(argv + 2), NULL);
+        execv((argv+2)[0], (argv+2));
+		//execl(exefile_name, exefile_name, NULL);
 	} 
     else 
     {
@@ -270,17 +265,15 @@ pid_t run_target(const char* exefile_name, char** argv){
 
 int main(int argc, char** argv)
 {
-    char* func_name = argv[1];
-    char* exefile_name = argv[2];
+    const char* func_name = argv[1];
+    const char* exefile_name = argv[2];
     Elf64_Half sec_index_of_func = SHN_UNDEF;
     unsigned long addr;
     if (!check_file_and_func(exefile_name, func_name, &sec_index_of_func, &addr)) 
     {
         return 0;
     }
-
-
-    pid_t child_pid = run_target(exefile_name, argv);
+    pid_t child_pid = run_target(argv);
     run_sys_debugger(child_pid, addr, sec_index_of_func, exefile_name, func_name);
     return 0;
 }
